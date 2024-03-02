518b4f328b131ab7d89e1b0ca72f651679202fe5
yeshwanthpolipilli <yeshwanthpolipilli@gmail.com>
2024-03-02 13:11:31 +0530

    removed eval

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -4,6 +4,7 @@ import '../Style.css'
 
 function Calculator() {
   const [value,setValue]= useState('');
+  const yes=eval;
   return (
  <>
 
@@ -43,7 +44,7 @@ function Calculator() {
 <input className='hey' type="button" value="0" onClick={e => setValue(value + e.target.value) }/>
 <input className='hey' type="button" value="00" onClick={e => setValue(value + e.target.value) }/>
   <input className='hey' type="button" value="." onClick={e => setValue(value + e.target.value) }/>
-  <input className='last' type="button" value="=" onClick={e => setValue(eval(value))} />
+  <input className='last' type="button" value="=" onClick={e => setValue(yes(value))} />
 
 </div>
 
